0493ae3c38b83c3b34a504fb7dfd3b7b0d4c3a61
Corey <corey.hampton@student.awesomeinc.org>
2023-06-01 16:40:23 +0000

    update navbar with major changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export const Home = () => {
+export default function Home({ fixed }) {
   return (
-    <div className='hero text-white text-4xl md:text-6xl justify-center '>
+    <div className='relative hero h-screen text-white text-4xl md:text-6xl justify-center bg-red-950 py-10'>
       <h1 className='text-4xl md:text-6xl'>Welcome to Saturn University</h1>
       <p className='text-2xl md:text-3xl'>"Where Stellar Minds Soar Across the Rings!"</p>
 
